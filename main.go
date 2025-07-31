8d81323ed0fa4a4cf9dd2f7b128479fab3d69c97
lucasrodlima <lucasrodlima27@gmail.com>
2025-07-31 00:23:51 -0300

    login command logic implemented

--- main.go
+++ main.go
@@ -3,7 +3,50 @@ package main
 import (
 	"fmt"
 	"github.com/lucasrodlima/rss_aggregator/internal/config"
+	"os"
 )
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.handlers[cmd.name](s, cmd)
+	if err != nil {
+		fmt.Println("Error running command handler function")
+		return err
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(s *state, cmd command) error) {
+	c.handlers[name] = f
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) != 3 {
+		return fmt.Errorf("Login must have one argument (username)")
+	}
+
+	newUsername := cmd.args[2]
+
+	err := s.config.SetUser(newUsername)
+	if err != nil {
+		return fmt.Errorf("Error setting new user")
+	}
+
+	fmt.Printf("Login successful as %s!\n", newUsername)
+	return nil
+}
 
 func main() {
 	sysConfig, err := config.Read()
@@ -12,13 +55,29 @@ func main() {
 		return
 	}
 
-	err = sysConfig.SetUser("lucasrodlima")
-	if err != nil {
-		fmt.Println(err)
+	currentState := state{
+		config: sysConfig,
+	}
+
+	currentCommands := commands{
+		handlers: make(map[string]func(*state, command) error),
+	}
+
+	currentCommands.register("login", handlerLogin)
+
+	currentArgs := os.Args
+	if len(currentArgs) < 2 {
+		fmt.Println("Less than 2 arguments")
 		return
 	}
 
-	fmt.Printf("Config file contents:\n\nDbUrl: %s\nUsername: %s\n",
-		sysConfig.DbUrl,
-		sysConfig.CurrentUserName)
+	currentCommand := command{
+		name: currentArgs[1],
+		args: currentArgs,
+	}
+
+	err = currentCommands.run(&currentState, currentCommand)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
